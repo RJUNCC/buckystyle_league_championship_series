c10cc43c96def506bfb0f2f46f4a02794639f31d
R J <20bigal04@ryeguytech.com>
2025-03-12 01:19:27 -0400

    added

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -15,6 +15,7 @@ from cogs.scheduling import SchedulingCog
 from cogs.season_management import SeasonManagementCog
 from cogs.playoff_management import PlayoffManagementCog
 from cogs.season_summary import SeasonSummaryCog
+from cogs.send_images import ImageSenderCog
 from models.player import initialize_db
 
 load_dotenv()
@@ -33,7 +34,7 @@ class MyBot(discord.Bot):
         cogs = [
             AdminCog, AvailabilityCog, TeamManagementCog, SeriesManagementCog,
             StatisticsCog, SchedulingCog, SeasonManagementCog,
-            PlayoffManagementCog, SeasonSummaryCog
+            PlayoffManagementCog, SeasonSummaryCog, ImageSenderCog
         ]
         for cog in cogs:
             try:
