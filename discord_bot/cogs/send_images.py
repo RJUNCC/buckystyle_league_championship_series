f8059e0a694928c73d5a60b0143d041a556ca561
R J <20bigal04@ryeguytech.com>
2025-03-14 02:39:18 -0400

    added

--- discord_bot/cogs/send_images.py
+++ discord_bot/cogs/send_images.py
@@ -16,7 +16,7 @@ import asyncio
 import logging
 import sys
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 from config import Config
 
 config = Config()
